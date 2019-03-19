f7b5975ed0260835896db3283cf74fd3482df850
Ian <ian@Deans-MacBook-Pro.local>
2019-03-19 02:36:09 -0500

    messing around

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('This is a modal'),
-                );
-              });
-        },
-      ),
-    );
+    return Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Title'),
+              onChanged: (String value) {
+                setState(() {
+                  titleValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Description'),
+              maxLines: 4,
+              onChanged: (String value) {
+                setState(() {
+                  descriptionValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.number,
+              onChanged: (String value) {
+                setState(() {
+                  priceValue = double.parse(value);
+                });
+              },
+            ),
+            Text(titleValue)
+          ],
+        ));
   }
 }
